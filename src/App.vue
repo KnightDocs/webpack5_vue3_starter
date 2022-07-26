e964ba9b1222c3581c60c6f9d0ecfa83c3086868
ChenPinchao <pinchao.chen@hongpucorp.com>
2022-07-26 10:50:35 +0800

    refactor(app.vue): delete redundant codes

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,6 @@
-<script setup lang="ts">
-import CurrentUser from './components/CurrentUser.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <img alt="Vue logo" src="@/assets/logo.png" />
-  <CurrentUser />
-  <nav>
-    <router-link :to="{ name: 'Home' }">Home</router-link> |
-    <router-link :to="{ name: 'Axios' }">Axios</router-link> |
-    <router-link :to="{ name: 'Vuex' }">Vuex</router-link>
-  </nav>
   <router-view></router-view>
 </template>
 
